fc6e89dc0921c5875e1332d37c9f148b1efc23a1
bensonml <bensonml@nih.gov>
2020-12-08 13:38:52 -0500

    rebuilt with sphinx-multiversion and versioning.html

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -44,6 +44,12 @@ autosectionlabel_prefix_document = True
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
+html_sidebars = {
+    '**': [
+        'versioning.html',
+    ],
+}
+
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
